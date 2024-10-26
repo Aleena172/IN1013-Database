168eb43991fed695ffa5131232a28e028be6565c
Aleena172 <Aleena.Bhatti@city.ac.uk>
2024-10-26 21:34:30 +0100

    Add files via upload

--- HW 4/task1.sql
+++ HW 4/task1.sql
@@ -1,25 +1,25 @@
-mysql> DROP DATABASE IF EXISTS pet_database;
+ DROP DATABASE IF EXISTS pet_database;
 
-mysql> CREATE DATABASE pet_database;
+ CREATE DATABASE pet_database;
 
-mysql> USE pet_database;
+ USE pet_database;
 
-mysql> CREATE TABLE petPet (
-    -> petname VARCHAR(20) NOT NULL,
-    ->  owner VARCHAR(45) NOT NULL,
-    -> species VARCHAR(45),
-    -> gender ENUM('M', 'F') NULL,
-    -> birth DATE,
-    -> death DATE,
-    -> PRIMARY KEY (petname),
-    -> UNIQUE (petname)
-    -> );
+ CREATE TABLE petPet (
+     petname VARCHAR(20) NOT NULL,
+      owner VARCHAR(45) NOT NULL,
+     species VARCHAR(45),
+     gender ENUM('M', 'F') NULL,
+     birth DATE,
+     death DATE,
+     PRIMARY KEY (petname),
+     UNIQUE (petname)
+     );
 
-mysql> CREATE TABLE petEvent (
-    -> petname VARCHAR(20) NOT NULL,
-    -> eventdate DATE NOT NULL,
-    -> eventtype VARCHAR(15) NOT NULL,
-    -> remark TEXT(255),
-    -> PRIMARY KEY (petname, eventdate),
-    -> FOREIGN KEY (petname) REFERENCES petPet(petname)
-    -> );
+ CREATE TABLE petEvent (
+     petname VARCHAR(20) NOT NULL,
+     eventdate DATE NOT NULL,
+     eventtype VARCHAR(15) NOT NULL,
+     remark TEXT(255),
+     PRIMARY KEY (petname, eventdate),
+     FOREIGN KEY (petname) REFERENCES petPet(petname)
+     );
--- HW 4/task2.sql
+++ HW 4/task2.sql
@@ -1,50 +1,50 @@
-mysql> USE pet_database;
+USE pet_database;
 Database changed
 
-mysql> INSERT INTO petPet VALUES
-    ->     ("Fluffy", "Harold", "cat", "F", "1993-02-04", NULL),
-    ->     ("Claws", "Gwen", "cat", "M", "1994-03-17", NULL),
-    ->     ("Buffy", "Harold", "dog", "F", "1989-05-13", NULL),
-    ->     ("Fang", "Benny", "dog", "M", "1990-08-27", NULL),
-    ->     ("Bowser", "Diane", "dog", "M", "1979-08-31", "1995-07-29"),
-    ->     ("Chirpy", "Gwen", "bird", "F", "1998-09-11", NULL),
-    ->     ("Whistler", "Gwen", "bird", NULL, "1997-12-09", NULL),
-    ->     ("Slim", "Benny", "snake", "M", "1996-04-29", NULL),
-    ->     ('Puffball','Diane','hamster','F','1999-03-30',NULL);
+ INSERT INTO petPet VALUES
+     ("Fluffy", "Harold", "cat", "F", "1993-02-04", NULL),
+     ("Claws", "Gwen", "cat", "M", "1994-03-17", NULL),
+     ("Buffy", "Harold", "dog", "F", "1989-05-13", NULL),
+     ("Fang", "Benny", "dog", "M", "1990-08-27", NULL),
+     ("Bowser", "Diane", "dog", "M", "1979-08-31", "1995-07-29"),
+     ("Chirpy", "Gwen", "bird", "F", "1998-09-11", NULL),
+     ("Whistler", "Gwen", "bird", NULL, "1997-12-09", NULL),
+     ("Slim", "Benny", "snake", "M", "1996-04-29", NULL),
+     ('Puffball','Diane','hamster','F','1999-03-30',NULL);
 
-mysql> INSERT INTO petEvent VALUES
-    ->     ("Fluffy", "1995-05-15", "litter", "4 kittens, 3 female, 1 male"),
-    ->     ("Buffy", "1993-06-23", "litter", "5 puppies, 2 female, 3 male"),
-    ->     ("Buffy", "1994-06-19", "litter", "3 puppies, 3 female"),
-    ->     ("Chirpy", "1999-03-21", "vet", "needed beak straightened"),
-    ->     ("Slim", "1997-08-03", "vet", "broken rib"),
-    ->     ("Slim", "1997-10-04", "vet", "broken tooth"),
-    ->     ("Bowser", "1991-10-12", "kennel", NULL),
-    ->     ("Fang", "1991-10-12", "kennel", NULL),
-    ->     ("Fang", "1998-08-28", "birthday", "Gave him a new chew toy"),
-    ->     ("Claws", "1998-03-17","birthday", "Gave him a new flea collar"),
-    ->     ("Whistler", "1998-12-09", "birthday", "First birthday");
+ INSERT INTO petEvent VALUES
+         ("Fluffy", "1995-05-15", "litter", "4 kittens, 3 female, 1 male"),
+         ("Buffy", "1993-06-23", "litter", "5 puppies, 2 female, 3 male"),
+         ("Buffy", "1994-06-19", "litter", "3 puppies, 3 female"),
+         ("Chirpy", "1999-03-21", "vet", "needed beak straightened"),
+         ("Slim", "1997-08-03", "vet", "broken rib"),
+         ("Slim", "1997-10-04", "vet", "broken tooth"),
+         ("Bowser", "1991-10-12", "kennel", NULL),
+         ("Fang", "1991-10-12", "kennel", NULL),
+         ("Fang", "1998-08-28", "birthday", "Gave him a new chew toy"),
+         ("Claws", "1998-03-17","birthday", "Gave him a new flea collar"),
+         ("Whistler", "1998-12-09", "birthday", "First birthday");
 
     //make updates
-    mysql> INSERT INTO petEvent VALUES ('Fluffy', '2020-10-15', 'vet', 'antibiotics');
-    mysql> INSERT INTO petPet VALUES ('Hammy', 'Diane', 'hamster', 'M', '2010-10-30', NULL);
-    mysql> INSERT INTO petEvent VALUES ('Hammy', '2020-10-15', 'vet', 'antibiotics');
+INSERT INTO petEvent VALUES ('Fluffy', '2020-10-15', 'vet', 'antibiotics');
+INSERT INTO petPet VALUES ('Hammy', 'Diane', 'hamster', 'M', '2010-10-30', NULL);
+INSERT INTO petEvent VALUES ('Hammy', '2020-10-15', 'vet', 'antibiotics');
 
-mysql> UPDATE petEvent
-    -> SET remark = '5 kittens, 3 female, 2 male'
-    -> WHERE petname = 'Fluffy' AND eventdate = '1995-05-15' AND eventtype = 'litter';
+UPDATE petEvent
+     SET remark = '5 kittens, 3 female, 2 male'
+     WHERE petname = 'Fluffy' AND eventdate = '1995-05-15' AND eventtype = 'litter';
     
-mysql> UPDATE petEvent
-    ->  SET petname = 'Claws'
-    ->  WHERE eventdate = '1997-08-03' AND eventtype = 'vet' AND remark = 'broken rib';
+UPDATE petEvent
+      SET petname = 'Claws'
+      WHERE eventdate = '1997-08-03' AND eventtype = 'vet' AND remark = 'broken rib';
 
-mysql> UPDATE petPet
-    -> SET death = '2020-09-01'
-    -> WHERE petname = 'Puffball' AND owner ='Diane' AND species ='hamster' AND  gender ='F'AND birth ='1999-03-30';
+UPDATE petPet
+     SET death = '2020-09-01'
+     WHERE petname = 'Puffball' AND owner ='Diane' AND species ='hamster' AND  gender ='F'AND birth ='1999-03-30';
 
- mysql> DELETE FROM petPet WHERE petname = 'Buffy' AND owner = 'Harold' AND species ='dog' AND  gender ='F'AND birth ='1989-05-13';
- mysql> DELETE FROM petEvent WHERE petname = 'Buffy' AND eventdate = '1993-06-23' AND eventtype = 'litter'AND remark = '5 puppies, 2 female, 3 male';
- mysql>  DELETE FROM petEvent WHERE petname = 'Buffy' AND eventdate = '1994-06-19' AND eventtype = 'litter'AND remark = '3 puppies, 3 female';
+DELETE FROM petPet WHERE petname = 'Buffy' AND owner = 'Harold' AND species ='dog' AND  gender ='F'AND birth ='1989-05-13';
+DELETE FROM petEvent WHERE petname = 'Buffy' AND eventdate = '1993-06-23' AND eventtype = 'litter'AND remark = '5 puppies, 2 female, 3 male';
+DELETE FROM petEvent WHERE petname = 'Buffy' AND eventdate = '1994-06-19' AND eventtype = 'litter'AND remark = '3 puppies, 3 female';
 
  
 
